a96acf351deb8a000899c319ae824791427dceaf
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-04-27 17:00:34 +0800

    Initial commit

---
+++ RiggedDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiggedDiceApp.swift
+//  RiggedDice
+//
+//  Created by Liko Setiawan on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RiggedDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
